374c7f28ad5c34904b57e6f744d6b5c53169f317
MacBobby Chibuzor <theghostmac@gmail.com>
2022-05-20 23:06:59 +0100

    finished go file server

--- main.go
+++ main.go
@@ -10,4 +10,8 @@ func main() {
 	http.Handle("/", staticFileServer)
 
 	log.Println("Starting the web application on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
